8000a07811889ac9cb4d66dff7e969e3bc01ea84
michelypsouza <michely.psouza@gmail.com>
2022-02-12 18:26:21 -0300

    exercicio da aula 56 do curso de java basico e aula 4 do curso de java intermediario - calculadora enum

---
+++ src/com/loiane/cursojava/aula56/exercicio/Calculadora.java
@@ -0,0 +1,36 @@
+package com.loiane.cursojava.aula56.exercicio;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+
+        System.out.println("Nova operação com 2 elementos: ");
+
+        double a = 2.0;
+        double b = 3.0;
+
+        for (OperacaoMatematica om : OperacaoMatematica.values()) {
+            System.out.print(a + " ");
+            System.out.print(om.toString() + " ");
+            System.out.print(b + " = ");
+            System.out.println(om.executarOperacao(a,b));
+        }
+
+        System.out.println("Nova operação com 3 elementos: ");
+
+        double x = 6.0;
+        double y = 3.0;
+        double z = 1.0;
+
+        for (OperacaoMatematica om : OperacaoMatematica.values()) {
+            System.out.print(x + " ");
+            System.out.print(om.toString() + " ");
+            System.out.print(y + " ");
+            System.out.print(om.toString() + " ");
+            System.out.print(z + " = ");
+            System.out.println(om.executarOperacao(x,y,z));
+        }
+
+    }
+
+}
---
+++ src/com/loiane/cursojava/aula56/exercicio/OperacaoMatematica.java
@@ -0,0 +1,80 @@
+package com.loiane.cursojava.aula56.exercicio;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum OperacaoMatematica {
+
+    SOMA('+') {
+        @Override
+        public Double executarOperacao(Double... variaveis) {
+            List<Double> listaVariaveis = converterParaLista(variaveis);
+            Double resultado = 0.0;
+            for (Double var : listaVariaveis) {
+                int indice = listaVariaveis.indexOf(var);
+                resultado = (indice == 0) ? var : resultado + var;
+            }
+            return resultado;
+        }
+    }, SUBTRACAO('-') {
+        @Override
+        public Double executarOperacao(Double... variaveis) {
+            List<Double> listaVariaveis = converterParaLista(variaveis);
+            Double resultado = 0.0;
+            /*
+            for (int i = 0; i < listaVariaveis.size(); i++) {
+                Double var = listaVariaveis.get(i);
+                if (i == 0) {
+                    resultado = var;
+                } else {
+                    resultado = resultado - var;
+                }
+            }
+            */
+            for (Double var : listaVariaveis) {
+                int indice = listaVariaveis.indexOf(var);
+                resultado = (indice == 0) ? var : resultado - var;
+            }
+            return resultado;
+        }
+    }, MULTIPLICACAO('*') {
+        @Override
+        public Double executarOperacao(Double... variaveis) {
+            List<Double> listaVariaveis = converterParaLista(variaveis);
+            Double resultado = 0.0;
+            for (Double var : listaVariaveis) {
+                int indice = listaVariaveis.indexOf(var);
+                resultado = (indice == 0) ? var : resultado * var;
+            }
+            return resultado;
+        }
+    }, DIVISAO('/') {
+        @Override
+        public Double executarOperacao(Double... variaveis) {
+            List<Double> listaVariaveis = converterParaLista(variaveis);
+            Double resultado = 0.0;
+            for (Double var : listaVariaveis) {
+                int indice = listaVariaveis.indexOf(var);
+                resultado = (indice == 0) ? var : resultado / var;
+            }
+            return resultado;
+        }
+    };
+
+    private char simbolo;
+
+    OperacaoMatematica(char simbolo) {
+        this.simbolo = simbolo;
+    }
+
+    public abstract Double executarOperacao(Double... variaveis);
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.simbolo);
+    }
+
+    private static List<Double> converterParaLista(Double... variaveis) {
+        return Arrays.asList(variaveis);
+    }
+}

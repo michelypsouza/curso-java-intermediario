236d95cbeff5cbbe654c1547e52ea9d70ece8ee0
Michely Souza <michely.souza@sefin.fortaleza.ce.gov.br>
2022-02-13 12:20:13 -0300

    aula 65 do curso de java basico e aula 13 do curso de java intermediario

---
+++ src/com/loiane/cursojava/aula65/Teste.java
@@ -0,0 +1,19 @@
+package com.loiane.cursojava.aula65;
+
+@InformacaoAula(
+        autor = "Loiane Groner",
+        aulaNumero = 65,
+        blog = "loiane.com"
+)
+public class Teste {
+
+    @InformacaoAula(
+            autor = "Loiane Groner",
+            aulaNumero = 65,
+            blog = "loiane.com"
+    )
+    public static void main(String[] args) {
+
+    }
+
+}

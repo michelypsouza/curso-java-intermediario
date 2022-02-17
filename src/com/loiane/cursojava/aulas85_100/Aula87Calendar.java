05c5305310c016ffe879485ab56b136181a17c6b
michelypsouza <michely.psouza@gmail.com>
2022-02-17 13:39:47 -0300

    aula 87 do curso de java basico e intermediario

---
+++ src/com/loiane/cursojava/aulas85_100/Aula87Calendar.java
@@ -0,0 +1,33 @@
+package com.loiane.cursojava.aulas85_100;
+
+import java.util.Calendar;
+
+public class Aula87Calendar {
+
+    public static void main(String[] args) {
+
+        Calendar hoje = Calendar.getInstance(); //singleton
+
+        System.out.println(hoje);
+
+        int ano = hoje.get(Calendar.YEAR);
+        int mes = hoje.get(Calendar.MONTH);
+        int dia = hoje.get(Calendar.DAY_OF_MONTH);
+        int hora = hoje.get(Calendar.HOUR_OF_DAY);
+        int minutos = hoje.get(Calendar.MINUTE);
+        int segundos = hoje.get(Calendar.SECOND);
+
+        System.out.println(Calendar.JANUARY);
+        System.out.println(Calendar.FEBRUARY);
+
+        System.out.printf("Hoje é: %02d/%02d/%d %02d:%02d:%02d",
+                dia, (mes+1), ano, hora, minutos, segundos);
+
+        hoje.add(Calendar.DAY_OF_MONTH,-1);
+        hoje.add(Calendar.DAY_OF_MONTH,6);
+        System.out.println();
+        System.out.println(hoje.get(Calendar.DAY_OF_MONTH));
+
+    }
+
+}

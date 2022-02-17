6fe21cd5cf24cb2a77bd76e0cb1da5eb68336520
michelypsouza <michely.psouza@gmail.com>
2022-02-16 22:27:34 -0300

    aula 81 do curso de java basico e intermediario

---
+++ src/com/loiane/cursojava/aula75_84/AUla81.java
@@ -0,0 +1,19 @@
+package com.loiane.cursojava.aula75_84;
+
+public class AUla81 {
+
+    public static void main(String[] args) {
+
+        String teste = "Teste";
+
+        String testeMinusculo = teste.toLowerCase();
+        String testeMaiusculo = teste.toUpperCase();
+
+        System.out.println(testeMinusculo); //minusculo
+        System.out.println(testeMaiusculo); //maiusculo
+
+        //if (teste.toLowerCase().equals(testeMaiusculo.toLowerCase()))
+
+    }
+
+}

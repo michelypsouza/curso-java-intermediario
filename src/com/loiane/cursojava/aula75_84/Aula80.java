bca6945aaa2eef0ae08d5ddd2cce8d378303e9b0
michelypsouza <michely.psouza@gmail.com>
2022-02-16 22:22:54 -0300

    aula 80 do curso de java basico e intermediario

---
+++ src/com/loiane/cursojava/aula75_84/Aula80.java
@@ -0,0 +1,33 @@
+package com.loiane.cursojava.aula75_84;
+
+public class Aula80 {
+
+    public static void main(String[] args) {
+
+        String teste = "Isso é um teste.";
+
+        System.out.println(teste);
+        System.out.println(teste.substring(10));
+        System.out.println(teste.substring(10,15));
+
+        String ola = "olá ";
+        String mundo = "mundo";
+        String olaMundo = ola.concat(mundo); // mesma coisa que = ola + mundo
+        System.out.println(olaMundo);
+
+        String espacos = "i s p a ç o";
+        String semEspacoes = espacos.replace('i','e');
+        System.out.println(semEspacoes);
+
+        semEspacoes = semEspacoes.replaceAll(" ", "");
+        System.out.println(semEspacoes);
+
+        String nome = " meu nome é: ";
+        System.out.println(nome);
+        System.out.println(nome.trim());
+
+        //web services
+
+    }
+
+}
